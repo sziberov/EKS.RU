92036a9e14bd1cb392dbe2df67f4a1ac400f4a83
KAYOver <sziberov@gmail.com>
2017-04-04 14:38:06 +0300

    ...

--- 1.php
+++ 1.php
@@ -9,7 +9,7 @@ $max_pages = ceil($img_count / $per_page);
 $show = array_slice($images, $per_page * intval($_GET['page']) - 1, $per_page);
 
 if($_GET['page'] > 1)
-    $prev_link = '<a href="images.php?page='.($_GET['page']-1).'"> previous </a>';
+    echo '<a href="1.php?page='.($_GET['page']-1).'"> previous </a>';
 if($_GET['page'] < $max_pages)
-    $next_link = '<a href="images.php?page='.($_GET['page']+1).'"> next </a>';
+    echo '<a href="1.php?page='.($_GET['page']+1).'"> next </a>';
 ?>
